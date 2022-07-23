f4fe6c794cad204d26b76f7cced59a4a5b5361d4
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-23 09:58:02 -0500

    Create punteros.c

---
+++ punteros.c
@@ -0,0 +1,15 @@
+#include <stadio.h>
+
+int main(){
+
+  int identificador;
+  int *p_indentificador = &identificador;
+
+  char letraDNI;
+  char *p_letraDNI = &letraDNI;
+
+  float sueldoAnual;
+  floar *p_sueldoAnual = &sueldoAnual;
+
+  return 0;
+}
